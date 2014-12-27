e294a150b92cb6c904e4dca686569a6236a5ab72
urokomori <urokomori@gmail.com>
2014-12-27 11:19:37 +0900

    [Add] Add the clear button.

--- script/main.js
+++ script/main.js
@@ -1,35 +1,65 @@
 $(function() {
-  var canvas = document.getElementById('canvas');
-  if(!canvas || !canvas.getContext)return false;
-  var context = canvas.getContext('2d');
+  var noteCanvas = document.getElementById('note');
+  if(!noteCanvas || !noteCanvas.getContext){
+    return false
+  };
+  var noteContext = noteCanvas.getContext('2d');
+
+  var penCanvas = document.getElementById('pen');
+  if(!penCanvas || !penCanvas.getContext){
+    return false;
+  }
+
+  var penContext = penCanvas.getContext('2d');
+
+  var image = new Image();
+
+  image.onload = function() {
+    penContext.drawImage(image, 0, 0);
+  };
+
+  image.src = "image/pen.png";
+
+  $(penCanvas).draggable({ containment: '#screen',
+                          scroll: false });
      
-  var startX,
-      startY,
-      x,
-      y,
-      borderWidth = 10,
-      isDrawing = false;
- 
-  $('canvas').mousedown(function(e){
-    isDrawing = true;
-    startX = e.pageX - $(this).offset().left - borderWidth;
-    startY = e.pageY - $(this).offset().top - borderWidth;
+  var start = new Position(0,0);
+  var end = new Position(0,0);
+  var isDrawing = false;
+  var noteOffset = new Position($(noteCanvas).offset().left, $(noteCanvas).offset().top);
+
+  $(penCanvas).mousedown(function(e){
+      isDrawing = true;
+      start.x = e.pageX - noteOffset.x;
+      start.y = e.pageY - noteOffset.y;
     })
   .mousemove(function(e){
-    if(!isDrawing) return;
-    x = e.pageX - $(this).offset().left - borderWidth;
-    y = e.pageY - $(this).offset().top - borderWidth;
-    context.beginPath();
-    context.moveTo(startX, startY);
-    context.lineTo(x,y);
-    context.stroke();
-    startX = x;
-    startY =y;
-  })
+    
+    if(!isDrawing) {
+      return;
+    }
+
+    end.x = e.pageX - noteOffset.x;
+    end.y = e.pageY - noteOffset.y;
+    noteContext.beginPath();
+    noteContext.moveTo(start.x, start.y);
+    noteContext.lineTo(end.x,end.y);
+    noteContext.stroke();
+    start.x = end.x;
+    start.y = end.y;
+   })
   .mouseup(function(){
     isDrawing = false;
   })
-  .mouseleave(function(){
-    isDrawing =false;
-  });
- })
+
+  $('#clear-button').click(function(){
+      noteContext.clearRect(0, 0, noteCanvas.width, noteCanvas.height);
+    })
+
+});
+
+function Position(x,y){
+  this.x = x;
+  this.y = y;
+  return this;
+}
